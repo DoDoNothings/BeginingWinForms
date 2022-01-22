73a4c60acfcc021cbc87142a0a99434846a64637
Vavi_sq66 <Vavi_sq66@student.itstep.org>
2022-01-22 12:28:28 +0200

    star

--- WinForms_01_HelloWorld/Program.cs
+++ WinForms_01_HelloWorld/Program.cs
@@ -3,17 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using NLog;
 
 namespace WinForms_01_HelloWorld
 {
     static class Program
     {
+        public static NLog.Logger Logger;
+        private static void LoggerConfig()
+        {
+            Logger = NLog.LogManager.GetCurrentClassLogger();
+
+        }
+
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            LoggerConfig();
+            Program.Logger.Info("Fictions");
+            
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
